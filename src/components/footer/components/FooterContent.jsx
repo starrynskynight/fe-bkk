11084826ba97ac5736ede49bd23e7b9f7b572ce1
CodeWithTr <tita08rofiqoh06@gmail.com>
2025-11-11 22:00:04 +0700

    revisi-footer

--- src/components/footer/components/FooterContent.jsx
+++ src/components/footer/components/FooterContent.jsx
@@ -1,123 +1,65 @@
-import React from 'react'
-import { MdOutlineEmail } from "react-icons/md";
-import { IoCallOutline, IoLocationOutline } from "react-icons/io5";
-import { MENU_ITEMS } from "../../navbar/helper/NavItems";
-import { Link } from "react-router-dom";
+import React from "react";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import { PiXLogoBold } from "react-icons/pi";
-import { FaYoutube, FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const FooterContent = () => {
-   const footerLinks = MENU_ITEMS.map((item) => {
-    if (item.route) return item;
-
-    if (item.id === "aplikasi") {
-      return { ...item, route: "/aplikasi" };
-    }
-
-    return null;
-  }).filter(Boolean);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-6 text-center md:text-left inter">
-      <div className="md:col-span-1 px-4">
-          <h1 className="font-bold mb-3 md:mb-6">
-          TENTANG <span className="text-primary-red">MI<span className="text-primary-orange">JURNAL</span></span>
-          </h1>
-          <p className="text-[#6B6B6B] leading-[32px]">
-          MiJurnal hadir untuk memudahkan guru dan siswa dalam mencatat,
-          mengelola, dan mengikuti proses belajar mengajar secara lebih
-          teratur dan efisien
-          </p>
+    <footer className="bg-[#0E1C56] text-white py-10 px-6 md:px-16">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-8 mb-10">
+        <div className="flex items-center col-span-2">
+          <h2 className="text-[36px] font-semibold mb-4">
+            Terhubung Bersama <br />
+            <span className="font-bold text-white">
+              BKK SMKN 1 Purwosari
+            </span>
+          </h2>
+        </div>
+
+        <div>
+          <h3 className="font-semibold text-lg mb-4">Menu</h3>
+          <ul className="space-y-2 text-[#EAEAEA]">
+            <li>Home</li>
+            <li>Lowongan</li>
+            <li>Survey Kepuasan</li>
+            <li>Kontak</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="font-semibold text-lg mb-4">Informasi</h3>
+          <ul className="space-y-2 text-[#EAEAEA] mb-4">
+            <li>Berita</li>
+            <li>Gallery</li>
+            <li>Mitra Kerja</li>
+            <li>Tentang Kami</li>
+          </ul>
+
+          
+        </div>
+        <div className="flex items-center gap-4 text-[#F4B400] text-xl">
+            <a href="#" className="hover:text-yellow-300">
+              <FaFacebookF />
+            </a>
+            <a href="#" className="hover:text-yellow-300">
+              <PiXLogoBold />
+            </a>
+            <a href="#" className="hover:text-yellow-300">
+              <FaInstagram />
+            </a>
+          </div>
       </div>
 
-      <div className="md:col-span-1 px-8">
-        <h1 className="font-bold mb-3 md:mb-6">AKSI CEPAT</h1>
-        <ul className="space-y-2 md:space-y-4 text-black font-semibold">
-          {footerLinks.map((item) => (
-            <li key={item.id}>
-              <Link
-                to={item.route}
-                className="relative block cursor-pointer pl-0 transition-all duration-300 pb-1 hover:pl-2 
-                          after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 
-                          after:bg-primary-red after:transition-all after:duration-300 hover:after:w-full"
-              >
-                {item.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
+      <div className="flex justify-between border-t border-[#FFC107] px-6 ">
+        <div className="flex items-center gap-3 mt-4">
+          <img src='/images/logo-bkk.png' alt="BKK Logo" className="w-12 h-12" />
+          <p className="font-semibold">BKK SMKN 1 PURWOSARI</p>
+        </div>
+        <div className="text-sm text-center text-gray-300 mt-4">
+          © 2025 BKK by SMKN 1 Purwosari • Dibuat oleh tim PKL GreatCode, 3PM, dan Hummatech
+        </div>
       </div>
+    </footer>
+  );
+};
 
-    <div className="md:col-span-1 px-4 text-center md:text-left">
-      <h1 className="font-bold mb-3 md:mb-6">ALAMAT KAMI</h1>
-
-      <div className="flex items-start gap-3 mb-4 text-black text-sm justify-center md:justify-start hover:text-primary-red transition-colors duration-200">
-        <IoLocationOutline className="text-primary-red flex-none w-6 h-6" />
-        <p>
-          Perum Permata Regency 1, Blk. 10 No.28, Perun Gpa, Ngijo, Kec. Karang
-          Ploso, Kabupaten Malang, Jawa Timur 65152
-        </p>
-      </div>
-
-      <div className="flex items-center gap-3 mb-4 text-black text-sm justify-center md:justify-start hover:text-primary-red transition-colors duration-200">
-        <MdOutlineEmail className="text-primary-red w-6 h-6" />
-        <p>mijurnal@gmail.com</p>
-      </div>
-
-      <div className="flex items-center gap-3 text-black text-sm justify-center md:justify-start hover:text-primary-red transition-colors duration-200">
-        <IoCallOutline className="text-primary-red w-6 h-6" />
-        <p>+1 386-688-3295</p>
-      </div>
-    </div>
-
-    <div className="md:col-span-1 px-4">
-      <h1 className="font-bold mb-3 md:mb-6">PLATFORM MEDIA</h1>
-      <p className="text-sm text-black mb-3">Bergabung dengan kami</p>
-
-      <div className="flex gap-4 text-2xl text-black justify-center md:justify-start ">
-        <a 
-          href="https://www.youtube.com/@hummatech" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="hover:text-primary-red transition-all duration-200 ease-in-out hover:-translate-y-0.5"
-        >
-          <FaYoutube />
-        </a>
-        <a 
-          href="https://www.facebook.com/hummatech/" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="hover:text-primary-red transition-all duration-200 ease-in-out hover:-translate-y-0.5"
-        >
-          <FaFacebook />
-        </a>
-        <a 
-          href="https://x.com/hummatech" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="hover:text-primary-red transition-all duration-200 ease-in-out hover:-translate-y-0.5"
-        >
-          <PiXLogoBold />
-        </a>
-        <a 
-          href="https://www.instagram.com/hummatech/" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="hover:text-primary-red transition-all duration-200 ease-in-out hover:-translate-y-0.5"
-        >
-          <FaInstagram />
-        </a>
-        <a 
-          href="https://www.linkedin.com/company/cv-hummasoft-komputindo/" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="hover:text-primary-red transition-all duration-200 ease-in-out hover:-translate-y-0.5"
-        >
-          <FaLinkedin />
-        </a>
-      </div>
-    </div>
-  </div>
-  )
-}
-
-export default FooterContent
+export default FooterContent;
